2a1b72409d5d288ef4cdca97788e3be9324b85a0
Kei-K23 <keiksl2301@gmail.com>
2024-08-12 19:05:24 +0630

    Feature section animation

--- src/components/FeatureItem.tsx
+++ src/components/FeatureItem.tsx
@@ -1,20 +1,77 @@
+"use client";
 import { DotLottiePlayer } from "@dotlottie/react-player";
-import React from "react";
+import {
+  motion,
+  animate,
+  useMotionTemplate,
+  useMotionValue,
+  ValueAnimationTransition,
+} from "framer-motion";
+import React, { useEffect, useRef } from "react";
 
 export default function FeatureItem({
   icon,
   title,
   isNew,
+  active,
+  handleClick,
 }: {
   icon: string;
   title: string;
   isNew: boolean;
+  active: boolean;
+  handleClick: () => void;
 }) {
+  const featureItemRef = useRef<HTMLDivElement | null>(null);
+  const xPercentage = useMotionValue(0);
+  const yPercentage = useMotionValue(0);
+
+  const maskImage = useMotionTemplate`radial-gradient(80px 80px at ${xPercentage}% ${yPercentage}%, black, transparent)`;
+
+  useEffect(() => {
+    if (!featureItemRef?.current || !active) return;
+
+    xPercentage.set(0);
+    yPercentage.set(0);
+
+    const { height, width } = featureItemRef.current.getBoundingClientRect();
+    const circumferences = height * 2 + width * 2;
+
+    const times = [
+      0,
+      width / circumferences,
+      (width + height) / circumferences,
+      (width * 2 + height) / circumferences,
+      1,
+    ];
+
+    const options: ValueAnimationTransition = {
+      times,
+      duration: 4,
+      repeat: Infinity,
+      repeatType: "loop",
+      ease: "linear",
+    };
+
+    animate(xPercentage, [0, 100, 100, 0, 0], options);
+    animate(yPercentage, [0, 0, 100, 100, 0], options);
+  }, [active]);
   return (
     <div
+      ref={featureItemRef}
       key={title}
-      className="flex-1 border border-white/15 py-2.5 px-3 md:px-5 flex items-center gap-4 md:gap-6 rounded-lg relative"
+      className="flex-1 border border-white/15 py-2.5 px-3 md:px-5 flex items-center gap-4 md:gap-6 rounded-lg relative cursor-pointer"
+      onClick={handleClick}
     >
+      {active && (
+        <motion.div
+          style={{
+            maskImage,
+          }}
+          className="absolute inset-0 border-2 border-[#A369FF] rounded-lg"
+        />
+      )}
+
       <div className="size-12 border border-white/10 rounded-lg inline-flex items-center justify-center">
         <DotLottiePlayer
           src={icon}
--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -3,8 +3,50 @@ import { TABS } from "../../constants";
 import Image from "next/image";
 import productImg from "@/assets/product-image.png";
 import FeatureItem from "@/components/FeatureItem";
+import { useState } from "react";
+import {
+  animate,
+  motion,
+  useMotionTemplate,
+  useMotionValue,
+  ValueAnimationTransition,
+} from "framer-motion";
 
 export const Features = () => {
+  const [selectedFeatureItem, setSelectedFeatureItem] = useState(0);
+
+  const bgPositionX = useMotionValue(TABS[0].backgroundPositionX);
+  const bgPositionY = useMotionValue(TABS[0].backgroundPositionY);
+  const bgSizeX = useMotionValue(TABS[0].backgroundSizeX);
+
+  const bgPosition = useMotionTemplate`${bgPositionX}% ${bgPositionY}%`;
+  const bgSize = useMotionTemplate`${bgSizeX}% auto`;
+
+  const handleSelectFeatureItem = (index: number) => {
+    setSelectedFeatureItem(index);
+    const animateOptions: ValueAnimationTransition = {
+      duration: 2,
+      ease: "easeInOut",
+    };
+
+    animate(
+      bgSizeX,
+      [bgSizeX.get(), 100, TABS[index].backgroundSizeX],
+      animateOptions
+    );
+
+    animate(
+      bgPositionX,
+      [bgPositionX.get(), TABS[index].backgroundPositionX],
+      animateOptions
+    );
+    animate(
+      bgPositionY,
+      [bgPositionY.get(), TABS[index].backgroundPositionY],
+      animateOptions
+    );
+  };
+
   return (
     <section id="feature" className="py-20 md:py-24">
       <div className="container">
@@ -16,17 +58,28 @@ export const Features = () => {
           revolutionized the way businesses approach SEO.
         </p>
         <div className="mt-10 mb-6 flex flex-col gap-y-2 lg:flex-row lg:items-center lg:justify-between lg:gap-3">
-          {TABS.map((tab) => (
+          {TABS.map((tab, index) => (
             <FeatureItem
               key={tab.title}
               title={tab.title}
               isNew={tab.isNew}
               icon={tab.icon}
+              active={index === selectedFeatureItem}
+              handleClick={() => {
+                handleSelectFeatureItem(index);
+              }}
             />
           ))}
         </div>
         <div className="border border-white/15 p-2.5 rounded-xl">
-          <Image src={productImg} alt="product image" />
+          <motion.div
+            className="aspect-video bg-cover border border-white/20 rounded-lg"
+            style={{
+              backgroundPosition: bgPosition,
+              backgroundSize: bgSize,
+              backgroundImage: `url(${productImg.src})`,
+            }}
+          />
         </div>
       </div>
     </section>
